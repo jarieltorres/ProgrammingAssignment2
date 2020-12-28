2d702e94b8b7cfa1aec58bba8e7acfc196b7ed02
jarieltorres <76669924+jarieltorres@users.noreply.github.com>
2020-12-27 22:21:55 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
  
-
+    invertir = NULL
+    set = function(matriz) {
+        x <<- matriz
+        invertir <<- NULL
+    }
+    get = function() x
+    
+    setinvertir = function(inverse) invertir <<- inverse 
+    getinvertir = function() invertir
+    
+    list(set=set, get=get, setinvertir=setinvertir, getinvertir=getinvertir)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
 
-
+    invertir = x$getinv()
+    
+    if (!is.null(invertir)){
+        message("Datos desde Cache")
+        return(invertir)
+    }
+    
+    datosMatriz = x$get()
+    invertir = solve(datosMatriz, ...)
+    
+    x$setinv(invertir)
+    
+    return(invertir)
 }
